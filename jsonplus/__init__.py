8942aba156b2585a14da7d9fcaaebdd021196c5d
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2013-09-22 22:17:11 +0200

    support for all datetime.* objects: added timedelta, date, time

--- jsonplus/__init__.py
+++ jsonplus/__init__.py
@@ -1,6 +1,6 @@
 """Custom datatypes (like datetime) serialization to/from JSON."""
 
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Radomir Stevanovic'
 __author_email__ = 'radomir.stevanovic@gmail.com'
 __copyright__ = 'Copyright 2013 Radomir Stevanovic'
@@ -8,25 +8,42 @@ __license__ = 'MIT'
 
 
 import simplejson as json
-from datetime import datetime
+from datetime import datetime, timedelta, date, time
 from dateutil.parser import parse as parse_datetime
+from functools import wraps
 
 
-def _datetime_for_json(value):
+def _isoformat_for_json(value):
     return {"__class__": type(value).__name__,
             "__value__": value.isoformat()}
 
+def _timedelta_for_json(value):
+    return {"__class__": type(value).__name__,
+            "__value__": {"days": value.days,
+                          "seconds": value.seconds,
+                          "microseconds": value.microseconds}}
 
 def _json_default(obj):
-    if isinstance(obj, datetime):
-        return _datetime_for_json(obj)
+    if isinstance(obj, datetime) or isinstance(obj, date) or isinstance(obj, time):
+        return _isoformat_for_json(obj)
+    elif isinstance(obj, timedelta):
+        return _timedelta_for_json(obj)
     raise TypeError(repr(obj) + " is not JSON serializable")
 
+
+def kwargified(constructor):
+    @wraps(constructor)
+    def kwargs_constructor(kwargs):
+        return constructor(**kwargs)
+    return kwargs_constructor
 
 def _json_object_hook(dict):
     classname = dict.get('__class__')
     handlers = {
-        'datetime': parse_datetime
+        'datetime': parse_datetime,
+        'timedelta': kwargified(timedelta),
+        'date': lambda v: parse_datetime(v).date(),
+        'time': lambda v: parse_datetime(v).timetz()
     }
     if classname:
         constructor = handlers.get(classname)
@@ -42,12 +59,10 @@ def json_dumps(*pa, **kw):
     kwupt.update(kw)
     return json.dumps(*pa, **kwupt)
 
-
 def json_loads(*pa, **kw):
     kwupt = {'object_hook': _json_object_hook}
     kwupt.update(kw)
     return json.loads(*pa, **kwupt)
-
 
 def json_prettydump(x, sort_keys=True):
     return json_dumps(x, sort_keys=sort_keys, indent=4*' ', separators=(',', ': '))
