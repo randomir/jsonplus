932c2ea64379e2350a288c4212034e68f7baf910
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-02-17 03:32:19 +0100

    added tests for JSONEncoder/JSONDecoder
    also, added a "complex" case test that tests encoding and decoding on a
    structure of non-basic types - all at once.

--- python/tests/test_compat.py
+++ python/tests/test_compat.py
@@ -6,6 +6,7 @@ sys.path.append(os.pardir)
 
 import unittest
 import jsonplus as json
+import simplejson
 import textwrap
 import math
 
@@ -51,17 +52,43 @@ class TestJSONPlus(unittest.TestCase):
                     2
                 ]
             }""")
+        
+        self.ts = datetime(2017, 2, 17, 2, 41, 4, 390605)
 
-        self.ts = datetime.now()
+        self.plus = [
+            self.ts, self.ts.date(), self.ts.time(), timedelta(10),
+            set(range(10)), frozenset(range(10)), 1+2j, 
+            Decimal('3.14'), Fraction(1, 3),
+            (1, 2, 3),
+            uuid.UUID('16ebeeb6-fc5f-4266-a3a9-50c320d87810')
+        ]
+        self.plus_dumps = \
+            '["2017-02-17T02:41:04.390605","2017-02-17","02:41:04.390605",'\
+            '864000.0,[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9],'\
+            '{"imag":2.0,"real":1.0},3.14,{"denominator":3,"numerator":1},'\
+            '[1,2,3],"16ebeeb6-fc5f-4266-a3a9-50c320d87810"]'
+
 
     def dump_and_load(self, val, **kwargs):
         return json.loads(json.dumps(val, **kwargs))
     
+    def encode_and_decode(self, val, **kwargs):
+        return json.loads(simplejson.dumps(val, cls=json.JSONEncoder, **kwargs), cls=json.JSONDecoder)
+    
+
     def test_basic_dumps(self):
         self.assertEqual(json.dumps(self.basic, sort_keys=True), self.basic_dumps)
     
+    def test_basic_encode(self):
+        self.assertEqual(
+            simplejson.dumps(self.basic, sort_keys=True, cls=json.JSONEncoder),
+            simplejson.dumps(self.basic, sort_keys=True))
+    
     def test_basic_loads(self):
         self.assertEqual(json.loads(self.basic_dumps), self.basic)
+    
+    def test_basic_decode(self):
+        self.assertEqual(simplejson.loads(self.basic_dumps, cls=json.JSONDecoder), self.basic)
 
     def test_basic_pretty(self):
         self.assertEqual(json.pretty(self.basic, sort_keys=True), self.basic_pretty)
@@ -70,6 +97,22 @@ class TestJSONPlus(unittest.TestCase):
         basic = json.loads(self.basic_dumps)
         self.assertEqual(json.dumps(basic, sort_keys=True), self.basic_dumps)
 
+
+    def test_basic_cycle(self):
+        self.assertEqual(self.dump_and_load(self.basic), self.basic)
+
+    def test_plus_dumps(self):
+        self.assertEqual(json.dumps(self.plus, sort_keys=True), self.plus_dumps)
+    
+    def test_plus_encode(self):
+        # specify separators manually, because JSONEncoder already receives 
+        # separators set from simplejson.dumps() to a different default 
+        # than we use in jsonplus.dumps()
+        self.assertEqual(
+            simplejson.dumps(self.plus, sort_keys=True, separators=(',', ':'), cls=json.JSONEncoder),
+            json.dumps(self.plus, sort_keys=True))
+
+    
     def test_datetime(self):
         self.assertEqual(self.dump_and_load(self.ts), self.ts.isoformat())
 
--- python/tests/test_exact.py
+++ python/tests/test_exact.py
@@ -6,6 +6,7 @@ sys.path.append(os.pardir)
 
 import unittest
 import jsonplus as json
+import simplejson
 import textwrap
 import math
 
@@ -52,16 +53,49 @@ class TestJSONPlus(unittest.TestCase):
                 ]
             }""")
 
-        self.ts = datetime.now()
+        self.ts = datetime(2017, 2, 17, 2, 41, 4, 390605)
+
+        self.plus = [
+            self.ts, self.ts.date(), self.ts.time(), timedelta(10),
+            set(range(10)), frozenset(range(10)), 1+2j, 
+            Decimal('3.14'), Fraction(1, 3),
+            (1, 2, 3),
+            uuid.UUID('16ebeeb6-fc5f-4266-a3a9-50c320d87810')
+        ]
+        self.plus_dumps = \
+            '[{"__class__":"datetime","__value__":"2017-02-17T02:41:04.390605"},'\
+            '{"__class__":"date","__value__":"2017-02-17"},'\
+            '{"__class__":"time","__value__":"02:41:04.390605"},'\
+            '{"__class__":"timedelta","__value__":{"days":10,"microseconds":0,"seconds":0}},'\
+            '{"__class__":"set","__value__":[0,1,2,3,4,5,6,7,8,9]},'\
+            '{"__class__":"frozenset","__value__":[0,1,2,3,4,5,6,7,8,9]},'\
+            '{"__class__":"complex","__value__":{"imag":2.0,"real":1.0}},'\
+            '{"__class__":"Decimal","__value__":"3.14"},'\
+            '{"__class__":"Fraction","__value__":{"denominator":3,"numerator":1}},'\
+            '{"__class__":"tuple","__value__":[1,2,3]},'\
+            '{"__class__":"UUID","__value__":{"hex":"16ebeeb6fc5f4266a3a950c320d87810"}}]'
+
 
     def dump_and_load(self, val, **kwargs):
-        return json.loads(json.dumps(val, **kwargs))
-    
+        return json.loads(json.json.dumps(val, cls=json.JSONEncoder, **kwargs))
+
+    def encode_and_decode(self, val, **kwargs):
+        return json.loads(simplejson.dumps(val, cls=json.JSONEncoder, **kwargs), cls=json.JSONDecoder)
+
+
     def test_basic_dumps(self):
         self.assertEqual(json.dumps(self.basic, sort_keys=True), self.basic_dumps)
+
+    def test_basic_encode(self):
+        self.assertEqual(
+            simplejson.dumps(self.basic, sort_keys=True, cls=json.JSONEncoder),
+            simplejson.dumps(self.basic, sort_keys=True))
     
     def test_basic_loads(self):
         self.assertEqual(json.loads(self.basic_dumps), self.basic)
+
+    def test_basic_decode(self):
+        self.assertEqual(simplejson.loads(self.basic_dumps, cls=json.JSONDecoder), self.basic)
 
     def test_basic_pretty(self):
         self.assertEqual(json.pretty(self.basic, sort_keys=True), self.basic_pretty)
@@ -69,6 +103,32 @@ class TestJSONPlus(unittest.TestCase):
     def test_basic_loads_dumps(self):
         basic = json.loads(self.basic_dumps)
         self.assertEqual(json.dumps(basic, sort_keys=True), self.basic_dumps)
+
+
+    def test_basic_cycle(self):
+        self.assertEqual(self.dump_and_load(self.basic), self.basic)
+    
+    def test_plus_cycle(self):
+        self.assertEqual(self.encode_and_decode(self.plus), self.plus)
+
+
+    def test_plus_dumps(self):
+        self.assertEqual(json.dumps(self.plus, sort_keys=True), self.plus_dumps)
+    
+    def test_plus_encode(self):
+        # specify separators manually, because JSONEncoder already receives 
+        # separators set from simplejson.dumps() to a different default 
+        # than we use in jsonplus.dumps()
+        self.assertEqual(
+            simplejson.dumps(self.plus, sort_keys=True, separators=(',', ':'), cls=json.JSONEncoder), 
+            json.dumps(self.plus, sort_keys=True))
+    
+    def test_plus_loads(self):
+        self.assertEqual(json.loads(self.plus_dumps), self.plus)
+    
+    def test_plus_decode(self):
+        self.assertEqual(simplejson.loads(self.plus_dumps, cls=json.JSONDecoder), self.plus)
+
 
     def test_datetime(self):
         self.assertEqual(self.dump_and_load(self.ts), self.ts)
