f0aeff2f037d664c9587df913415aea68db670bd
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-02-17 18:39:01 +0100

    flask simple demo app

--- python-flask/test/demo.py
+++ python-flask/test/demo.py
@@ -1,22 +1,26 @@
 from datetime import datetime
+from collections import namedtuple
+from fractions import Fraction
+from decimal import Decimal
 
 from flask import Flask, jsonify
+from flask_jsonplus import FlaskJSONPlus
 
 app = Flask(__name__)
+app.config['JSONPLUS_EXACT'] = True
 
-
-### jsonplus-driven jsonification
-
-from jsonplus import JSONEncoder, JSONDecoder
-
-app.json_encoder = JSONEncoder
-app.json_decoder = JSONDecoder
+FlaskJSONPlus(app)
 
 
 @app.route('/')
 def hello_world():
+    Point = namedtuple('Point', 'x y')
     data = {
-        'num': 1.23,
-        'now': datetime.now()
+        'third': Fraction(1, 3),
+        'dec': Decimal('0.1'),
+        'now': datetime.now(),
+        'set': set(range(3)),
+        'tuple': (3, 1, 4),
+        'namedtuple': Point(3, 4)
     }
     return jsonify(data)
