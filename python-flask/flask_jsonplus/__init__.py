abeb2034d0fa815b31048f8ca6fc887fd5c1c79c
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-02-17 18:35:16 +0100

    Flask extension for transparent usage of jsonplus: minimally functional version

--- python-flask/flask_jsonplus/__init__.py
+++ python-flask/flask_jsonplus/__init__.py
@@ -1,3 +1,32 @@
-from jsonplus import JSONEncoder, JSONDecoder
+import jsonplus
+from flask import current_app
 
-# TODO: attact JSONEncoder/JSONDecoder to app
+
+class JSONEncoder(jsonplus.JSONEncoder):
+    """Thin wrapper around :class:`jsonplus.JSONEncoder` that propagates
+    :class:`flask.current_app` config options to ``jsonplus`` encoder.
+    """
+
+    def __init__(self, **kw):
+        kw.setdefault('exact', current_app.config['JSONPLUS_EXACT'])
+        super(JSONEncoder, self).__init__(**kw)
+
+
+class FlaskJSONPlus(object):
+    """Flask-JSONPlus extension class."""
+
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        # if not specified in config, default to jsonplus' exact coding
+        app.config.setdefault('JSONPLUS_EXACT', True)
+
+        if not hasattr(app, 'extensions'):
+            app.extensions = dict()
+        app.extensions['jsonplus'] = self
+
+        app.json_encoder = JSONEncoder
+        app.json_decoder = jsonplus.JSONDecoder
