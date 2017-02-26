1907ea5c0f59979986a8e2a09b7955be13a0e3d8
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-02-27 00:01:36 +0100

    Flask-JSONPlus: bumped to v0.0.3

--- python-flask/setup.py
+++ python-flask/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Flask-JSONPlus',
-    version='0.0.2',
+    version='0.0.3',
     description="Flask extension for non-basic types' serialization to JSON via jsonplus lib.",
     long_description=open('README.rst').read(),
     author='Radomir Stevanovic',
