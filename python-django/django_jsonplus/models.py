7c26f27ae2ad9c8eaf3e5331d869f6c6f66b5a70
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-02-27 00:20:40 +0100

    django-jsonplus ext: minimal functionality (db field)

--- python-django/django_jsonplus/models.py
+++ python-django/django_jsonplus/models.py
@@ -1,3 +1,21 @@
+import jsonplus
+
 from django.db import models
 
-# Create your models here.
+
+class JSONPlusField(models.TextField):
+    """Use jsonplus serializer to support custom python types, like
+    `datetime`."""
+
+    def from_db_value(self, value, expression, connection, context):
+        if value is None:
+            return value
+        return jsonplus.loads(value)
+
+    def to_python(self, value):
+        if not isinstance(value, str):
+            return value
+        return jsonplus.loads(value)
+
+    def get_prep_value(self, value):
+        return jsonplus.dumps(value)
