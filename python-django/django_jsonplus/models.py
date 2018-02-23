84ba461dc6c6cbfcc78bf191e2677d3ede89bcc4
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2018-02-23 11:31:22 +0100

    Extend jsonplus to add custom djmoney serialization
    Testing new `jsonplus` feature of user type encoder/decoder, on a
    django-money extension of moneyed Money.

--- python-django/django_jsonplus/models.py
+++ python-django/django_jsonplus/models.py
@@ -22,3 +22,21 @@ class JSONPlusField(models.TextField):
         if value is None:
             return value
         return jsonplus.dumps(value)
+
+
+# additional Django-specific encoders
+
+from djmoney.money import Money as DjangoMoney
+
+# TODO: add to jsonplus a way to hook encoder/decoder with a priority
+# TODO: add to jsonplus unregister function
+# NOTE: the line below is an abomination; used for test/prototype only
+del jsonplus._encode_handlers['exact']['classname']['Money']
+
+@jsonplus.encoder('DjangoMoney', lambda obj: isinstance(obj, DjangoMoney))
+def _django_money_dumps(obj):
+    return jsonplus.getattrs(obj, attrs=['amount', 'currency'])
+
+@jsonplus.decoder('DjangoMoney')
+def _django_money_loads(val):
+    return DjangoMoney(**val)
