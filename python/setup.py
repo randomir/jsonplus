9d63e3999527f7d0fd3c46bc233f051651ac06ca
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2018-02-23 10:36:29 +0100

    started jsonplus v0.7.2

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='jsonplus',
-    version='0.7.1',
+    version='0.7.2-dev',
     description="Custom datatypes (like datetime) serialization to/from JSON.",
     long_description=open('README.rst').read(),
     author='Radomir Stevanovic',
