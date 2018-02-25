055f7d96800139382957486390d22ac826fbf7f6
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2018-02-25 10:47:28 +0100

    django ext: bump to v0.1.2
    support for (django-specific) user types, namely django-money

--- python-django/setup.py
+++ python-django/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-jsonplus',
-    version='0.1.1',
+    version='0.1.2',
     description="Django extension for non-basic types' serialization to JSON via jsonplus lib.",
     long_description=open('README.rst').read(),
     author='Radomir Stevanovic',
