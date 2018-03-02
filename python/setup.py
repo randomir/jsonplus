979bc17989d92fe8486a750dc60fb95cd140fd74
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2018-03-02 11:15:08 +0100

    Release jsonplus v0.8.0
    New: predicate-based prioritized encoders.

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='jsonplus',
-    version='0.7.2-dev',
+    version='0.8.0',
     description="Custom datatypes (like datetime) serialization to/from JSON.",
     long_description=open('README.rst').read(),
     author='Radomir Stevanovic',
