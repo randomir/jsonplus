32d179069da2b56567d211c1bec2bff95dc513be
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2018-03-03 10:34:22 +0100

    add decorator compat tests

--- python/tests/test_decorators.py
+++ python/tests/test_decorators.py
@@ -23,6 +23,17 @@ class TestDecorators(unittest.TestCase):
         self.assertEqual(jsonplus.dumps(mytype1(), sort_keys=True, exact=True),
                          '{"__class__":"mytype1","__value__":313}')
 
+    def test_user_encoder_compat(self):
+        class mytype1(object):
+            x = 313
+
+        @jsonplus.encoder('mytype1', exact=False)
+        def mytype_encoder(obj):
+            return obj.x
+
+        self.assertEqual(jsonplus.dumps(mytype1(), sort_keys=True, exact=False),
+                         '313')
+
     def test_user_decoder_exact(self):
         class mytype2(object):
             def __init__(self, val):
@@ -39,6 +50,19 @@ class TestDecorators(unittest.TestCase):
         a = mytype2(313)
         b = self.dump_and_load(a, exact=True)
         self.assertEqual(a.val, b.val)
+
+    def test_user_decoder_compat(self):
+        class mytype2(object):
+            def __init__(self, val):
+                self.val = val
+
+        @jsonplus.encoder('mytype2', exact=False)
+        def mytype_encoder(obj):
+            return obj.val
+
+        a = mytype2(313)
+        b = self.dump_and_load(a, exact=False)
+        self.assertEqual(a.val, b)
 
 
 if __name__ == '__main__':
